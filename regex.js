9d0e4e1267a653ea8f9ecefbd5e6f00455c8f2d8
Saugat Poudel <njsaugat@gmail.com>
2022-05-03 10:09:06 +0545

    regex practised and server CRUD app added

---
+++ regex.js
@@ -0,0 +1,3 @@
+const data='heello '
+
+console.log(data.match(/o/))
--- serverCrud.js
+++ serverCrud.js
@@ -1,9 +1,24 @@
 const http=require('http');
 const {getProducts}=require('./controller/controller.js');
+const {getProduct}=require('./controller/singleController.js')
+const {postProduct}=require('./controller/adder.js')
 const server=http.createServer((req,res)=>{
+    // let id= Math.floor(Math.random() * 5)+1
+    // console.log(id);
+    // const urlId=toString(id);
+    const urlId='3';
     if(req.url==='/products' && req.method==='GET'){
         getProducts(req,res);
-    }else{
+    }
+    // else if((req.url===`/product/${'1'}` || req.url===`/product/${'2'}` ||req.url===`/product/${'3'}` ||req.url===`/product/${'4'}` ||req.url===`/product/${'5'}` )&& req.method==='GET'){
+    else if(req.url.match(/\/product\/([0-9]+)/) &&req.method==='GET'){
+        const id=req.url.split('/')[2];
+        getProduct(req,res,id)
+    }
+    else if(req.url==='/products' && req.method==='POST'){
+        postProduct(req,res);
+    }
+    else{
         res.writeHead(404,{"Content-Type":"Application/JSON"})
         res.end(JSON.stringify({"message":"error occured in server"}))
     }
