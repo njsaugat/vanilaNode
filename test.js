e6c1b3314470c422dd10e9b1fac2b8239e3615a7
Saugat Poudel <njsaugat@gmail.com>
2022-05-04 07:46:56 +0545

    test is added so as to test the app

--- test.js
+++ test.js
@@ -21,8 +21,28 @@
 // values=[...values,value];
 // console.log(values);
 
-fs.writeFile('../data/products.json', JSON.stringify(newProduct),(err)=>{
-    if(err){
-        console.log(err);
-    }
+// fs.writeFile('../data/products.json', JSON.stringify(newProduct),(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+// })
+
+const EventEmitter=require('events');
+const eventEmitter=new EventEmitter();
+eventEmitter.on('start',(...number)=>{//This is REST Operator used to take multiple arguments
+    console.log(typeof number)
+    console.log(number)
+    console.log(`started ${number}`);
 })
+
+eventEmitter.emit('start', 23,32,233,32,3222,1);
+
+eventEmitter.on('play',(func)=>{
+    setTimeout(func,5000);
+    func();
+})
+eventEmitter.emit('play',()=>{
+    console.log('I am playing from play ')
+})
+// so basically .emit is like addEventListener ie triggering the event
+// and .on is like the under the hood implementation of the eventListener
