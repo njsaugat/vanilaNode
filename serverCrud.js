60924f557181fa2494219286c99c12b4b0db5364
Saugat Poudel <njsaugat@gmail.com>
2022-05-06 10:27:53 +0545

    server is updated

--- serverCrud.js
+++ serverCrud.js
@@ -1,7 +1,11 @@
 const http=require('http');
 const {getProducts}=require('./controller/controller.js');
 const {getProduct}=require('./controller/singleController.js')
-const {postProduct}=require('./controller/adder.js')
+const {postProduct}=require('./controller/adder.js');
+const { createDyanmicProduct } = require('./controller/createProductDynamic.js');
+const { createFinalProduct } = require('./controller/createProductFinal.js');
+const { updateProduct } = require('./controller/updateProduct.js');
+const { deleteProduct}=require('./controller/deleteProduct')
 const server=http.createServer((req,res)=>{
     // let id= Math.floor(Math.random() * 5)+1
     // console.log(id);
@@ -16,7 +20,17 @@ const server=http.createServer((req,res)=>{
         getProduct(req,res,id)
     }
     else if(req.url==='/products' && req.method==='POST'){
-        postProduct(req,res);
+        // postProduct(req,res);
+        // createDyanmicProduct(req,res)
+        createFinalProduct(req,res);
+    }
+    else if(req.url.match(/\/product\/([0-9]+)/) &&req.method==='PUT'){
+        const id=req.url.split('/')[2];
+        updateProduct(req,res,id)
+    }
+    else if(req.url.match(/\/product\/([0-9]+)/) &&req.method==='DELETE'){
+        const id=req.url.split('/')[2];
+        deleteProduct(req,res,id)
     }
     else{
         res.writeHead(404,{"Content-Type":"Application/JSON"})
