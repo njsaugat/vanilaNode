ec0849e199e8bb1ed3f8d7a635ab484cbbb3eb50
Saugat Poudel <njsaugat@gmail.com>
2022-05-07 11:21:29 +0545

    a test file to revise some imp conceps

--- test.js
+++ test.js
@@ -27,22 +27,31 @@
 //     }
 // })
 
-const EventEmitter=require('events');
-const eventEmitter=new EventEmitter();
-eventEmitter.on('start',(...number)=>{//This is REST Operator used to take multiple arguments
-    console.log(typeof number)
-    console.log(number)
-    console.log(`started ${number}`);
-})
+// const EventEmitter=require('events');
+// const eventEmitter=new EventEmitter();
+// eventEmitter.on('start',(...number)=>{//This is REST Operator used to take multiple arguments
+//     console.log(typeof number)
+//     console.log(number)
+//     console.log(`started ${number}`);
+// })
 
-eventEmitter.emit('start', 23,32,233,32,3222,1);
+// eventEmitter.emit('start', 23,32,233,32,3222,1);
 
-eventEmitter.on('play',(func)=>{
-    setTimeout(func,5000);
-    func();
-})
-eventEmitter.emit('play',()=>{
-    console.log('I am playing from play ')
-})
+// eventEmitter.on('play',(func)=>{
+//     setTimeout(func,5000);
+//     func();
+// })
+// eventEmitter.emit('play',()=>{
+//     console.log('I am playing from play ')
+// })
 // so basically .emit is like addEventListener ie triggering the event
 // and .on is like the under the hood implementation of the eventListener
+const arr=[1,2,3,4,5,6,7];
+const modArr=arr.map(element=>{
+    if(element<5){
+        return element=10000;
+    }
+})
+console.log(modArr)
+// console.log(arr)
+
