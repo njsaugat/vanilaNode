3e2cf78e1b0a21e67ac8658a5f169081bd3b2789
Saugat Poudel <njsaugat@gmail.com>
2022-12-31 19:26:40 +0545

    get data from file added

---
+++ utils/getDataFromApp.js
@@ -0,0 +1,18 @@
+function getDataFromUser(req){// so like this needs a request only
+    return new Promise((resolve,reject)=>{
+        try{
+            let body='';
+            req.on('data',(chunk)=>{
+                body+=chunk.toString();//converts to string
+            })
+            req.on('end',()=>{
+                resolve(JSON.parse(body));// so like we are resolving the body's data directly
+            })
+        }
+        catch(error){
+            reject(error);
+        }
+    })
+
+}
+module.exports={getDataFromUser}
